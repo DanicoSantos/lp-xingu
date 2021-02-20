df5fed79cf2e79441742975e45afddad9a23f5c2
Daniel Santos <dados.dan.santos@gmail.com>
2021-02-19 21:25:22 -0300

    feat: add form handler

---
+++ static/js/main.js
@@ -0,0 +1,33 @@
+var $form = $('form#subscription-form'), $submissionCompleteAlert = $('#submissionCompleteAlert'),
+    url = 'https://script.google.com/macros/s/AKfycbyySzf4RsZkM7Pbu2NTQEUNc35s26esBUTtk7lgG35JVjdV9yUt/exec';
+
+const confirmationAlert = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+Seus dados foram enviados com sucesso!
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+</button>
+</div>`;
+
+const loadingSpinner = `<div class="spinner-border text_primary align-self-center text-center" role="status">
+<span class="sr-only">Loading...</span>
+</div>`;
+
+const submissionComplete = () => {
+    $form.each(function(){
+        this.reset();
+    });
+
+    $submissionCompleteAlert.append(confirmationAlert);
+    $('.spinner-border').remove();
+}
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    beforeSend: () => $submissionCompleteAlert.append(loadingSpinner),
+    data: $form.serializeObject()   
+  }).done(() => submissionComplete());
+});
